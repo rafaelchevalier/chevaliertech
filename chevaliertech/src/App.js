16e0813c5e3f3f1b7666928351ec49181c61b187
RAFAEL CHEVALIER CARVALHO <rafaelchevalier@gmail.com>
2022-08-22 15:49:00 -0300

    Menu single page

--- chevaliertech/src/App.js
+++ chevaliertech/src/App.js
@@ -1,24 +1,19 @@
 import {useState} from 'react';
-import { Banner } from './componentes/banner/Banner';
+
 import { Menu } from './componentes/menu/Menu';
-import {Home} from './componentes/home/Home';
+
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="banner">
+      <header className="">
         <Menu />
-        <Banner />
+        
       </header>
       <body>
-      { clickHome() &&(
-          <Home />      
-      )}
           
-        
-        
       </body>
       
         
--- chevaliertech/src/componentes/menu/Menu.js
+++ chevaliertech/src/componentes/menu/Menu.js
@@ -1,29 +1,49 @@
 import {useState} from 'react';
-
+import { Banner } from '../banner/Banner';
+import {Home} from '../home/Home';
 import './menu.css';
 
 export const Menu = () =>{
     const [home,setHome] = useState();
+    const [banner,setBanner] = useState();
     const [portifolio,setPortifolio] = useState();
     const [contato,setContato] = useState();
     const [login,setLogin] = useState();
 
 
 function clickHome(){
-    setHome(!home);
-    
-    
+    setHome(!home); 
+}
+function clickBanner(){
+    setBanner(!banner); 
 }
     return(
         <div className="content_menu">
             <nav className="menu">
                 <ul>
                     <li className="menu-li" ><button type="button" onClick={clickHome} >Home</button></li>
+                    <li className="menu-li" ><button type="button" onClick={clickBanner} >Banner</button></li>
                     <li className="menu-li"><a href="#">Portifólio</a></li>
                     <li className="menu-li"><a href="#">Contato</a></li>
                     <li className="active"><a href="admin">LOGIN</a></li>
                 </ul>
             </nav>
+            
+            {banner && (
+                <div>
+                <Banner />
+                </div>
+            )}
+            
+            
+            {home && (
+                <div>    
+                <Home />
+                </div>
+            )}
+            
+            
+            
         </div>
         
     )
